4959efb77f17ca1d2b8d05f09afad7d38925b9eb
Joonas Griesinger <griesinger.joonas@gmail.com>
2023-12-05 22:40:49 +0100

    Preparing day 6 files.

---
+++ src/Solutions/Day06/Day6.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Solutions.Day06
+{
+    public class Day6 : DayBase
+    {
+        public override string Day { get; } = "06";
+    }
+}
---
+++ tests/AdventOfCode2023.Tests/Solutions/Day06/Day6Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Tests.Solutions.Day06
+{
+    public class Day6Tests
+    {
+    }
+}
